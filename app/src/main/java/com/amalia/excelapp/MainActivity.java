76d8df7e84a6999c6fad3b6e7c791625816f66ff
A.rasyid <amaliarasyid1999@gmail.com>
2020-02-18 08:22:50 +0700

    DOWNGRADE APACHE PO 4.1.2 to 3.9 for support below android 8

--- app/src/main/java/com/amalia/excelapp/MainActivity.java
+++ app/src/main/java/com/amalia/excelapp/MainActivity.java
@@ -13,24 +13,32 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<String> participantNames ;
-    private ArrayList<Integer> numbIdParticipants;
     private ArrayList<Participant> participants = new ArrayList<>();
     private final String TAG = "main act";
+    String[] headerColumns = {"No","NAMA","NIK","TANGGAL LAHIR",
+            "JK","NIP","JABATAN","GOL","TINGKAT PENDIDIKAN TERAKHIR",
+            "FAKULTAS/JURUSAN PEND TERAKHIR","TEMPAT KERJA","EMAIL",
+            "NO.HP","KODE POS","PARAF"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,58 +54,68 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(TAG,"clicked");
+                Log.d(TAG, "clicked");
                 createExcelFile();
 
             }
         });
     }
 
+    private void importFromExcel(){
+
+    }
+
     private void createExcelFile() {
 
         //creating workbook and a sheet
-        Workbook workbook = new HSSFWorkbook();
-        Sheet participantsSheet = workbook.createSheet("peserta diklat");
 
-//        //adjust column with to the content
-//        participantsSheet.autoSizeColumn(2);
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet participantsSheet = workbook.createSheet("peserta diklat");
+
 
         //create column and row
-        Row headerRow = participantsSheet.createRow(0);
+        HSSFRow headerRow = participantsSheet.createRow(0);
+
+        //create style font for header
+        HSSFFont font = workbook.createFont();
+        font.setFontName("Arial");
+        font.setBoldweight(Font.BOLDWEIGHT_BOLD);
+
+        HSSFCellStyle style = workbook.createCellStyle();
+        style.setAlignment(CellStyle.ALIGN_CENTER);
+        style.setFont(font);
+
         // declare a cell object reference
         Cell cell = null;
 
+        for(int i=0;i<headerColumns.length;i++){
+            cell = headerRow.createCell(i);
+            cell.setCellValue(headerColumns[i]);
+//            cell.setCellStyle(style);
+        }
 
-        cell = headerRow.createCell(0);
-        cell.setCellValue("No.");
-
-        cell = headerRow.createCell(1);
-        cell.setCellValue("Nama peserta");
-
-        cell = headerRow.createCell(2);
-        cell.setCellValue("NIK");
 
 
         //iterate data
-        int rowNum = 1;
-        for(Participant participant :participants ){
-            Row row = participantsSheet.createRow(rowNum++);
-            row.createCell(0).setCellValue(rowNum);
-            row.createCell(1).setCellValue(participant.getName());
-            row.createCell(2).setCellValue(participant.getId());
-
-        }
+//        int rowNum = 1;
+//        for (Participant participant : participants) {
+//            Row row = participantsSheet.createRow(rowNum++);
+//            row.createCell(0).setCellValue(rowNum);
+//            row.createCell(1).setCellValue(participant.getName());
+//            row.createCell(2).setCellValue(participant.get());
+//
+//        }
 
         //creating file excel
-        File file =  new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS),"daftar peserta diklat.xls");//Save into dir Documents
+        File file = new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "daftar peserta diklat.xls");//Save into dir Documents
         FileOutputStream outputStream = null;
         try {
             outputStream = new FileOutputStream(file);
             workbook.write(outputStream);
-            Log.d(TAG,"file created");
+            Log.d(TAG, "file created");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            Log.d(TAG," file not created");
+            Log.d(TAG, " file not created");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -105,23 +123,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void loadData(){
-        participantNames = new ArrayList<>();
-        participantNames.add("Amalia Salmi Rasyid");
-        participantNames.add("A.salmi rasyid");
-        participantNames.add("A.rasyid");
-        participantNames.add("salmi");
 
-        numbIdParticipants = new ArrayList<>();
-        numbIdParticipants.add(12345);
-        numbIdParticipants.add(54321);
-        numbIdParticipants.add(21435);
-        numbIdParticipants.add(54123);
+    private void loadData() {
+        Participant mParticipant = new Participant();
+        mParticipant.setName("Masni, SST");
+        mParticipant.setNikId("1409016412750001");
+        mParticipant.setNipId("19751224 200604 2 009");
+        mParticipant.setGender("Perempuan");
+        mParticipant.setBirthPlace("Seberang Pantai");
+        mParticipant.setBirthDate("24 Desember 1975");
+        mParticipant.setEmail("irwansyahmasni70@gmail.com");
+        mParticipant.setNumPhone("0853 6570 8289");
+        mParticipant.setPosition(new Position("Kepala Puskesmas","III/C"));
+        mParticipant.setEducation(new Education("S1","Kesehatan Masyarakat"));
 
-        //insert into list Participant
-        for(int i = 0 ; i<participantNames.size();i++){
-            participants.add(new Participant(participantNames.get(i),String.valueOf(numbIdParticipants.get(i))));
-        }
+        //insert into arraylist participants
+        participants.add(mParticipant);
+
 
     }
 
