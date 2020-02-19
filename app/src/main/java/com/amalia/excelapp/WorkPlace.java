d2f109ec13ffb84ff507787129c4c752f9745261
A.rasyid <amaliarasyid1999@gmail.com>
2020-02-19 09:40:16 +0700

    REMOVE property streetName

--- app/src/main/java/com/amalia/excelapp/WorkPlace.java
+++ app/src/main/java/com/amalia/excelapp/WorkPlace.java
@@ -2,13 +2,11 @@ package com.amalia.excelapp;
 
 public class WorkPlace {
     private String name;
-    private String streetName;
     private Address address;
     private int postalCode;
 
-    public WorkPlace(String name, String streetName, Address address, int postalCode) {
+    public WorkPlace(String name, Address address, int postalCode) {
         this.name = name;
-        this.streetName = streetName;
         this.address = address;
         this.postalCode = postalCode;
     }
@@ -21,13 +19,6 @@ public class WorkPlace {
         this.name = name;
     }
 
-    public String getStreetName() {
-        return streetName;
-    }
-
-    public void setStreetName(String streetName) {
-        this.streetName = streetName;
-    }
 
     public Address getAddress() {
         return address;
